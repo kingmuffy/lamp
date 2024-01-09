b309b9db9bc0355c9353a88d26bb593a03ab8107
Mac <kingmuffyatse@gmail.com>
2024-01-09 14:47:58 +0000

    d

---
+++ libs/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDQDlxDQ5_OF7MJZihEj66e--dbyhe_IiM",
+  authDomain: "lamp-b3187.firebaseapp.com",
+  projectId: "lamp-b3187",
+  storageBucket: "lamp-b3187.appspot.com",
+  messagingSenderId: "174893549791",
+  appId: "1:174893549791:web:828868b1ffa31fed73016a",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
