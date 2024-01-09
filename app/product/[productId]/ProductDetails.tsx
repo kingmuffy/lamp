f5b78765e8335e90620c89115ec6a6107256dee3
Mac <kingmuffyatse@gmail.com>
2024-01-09 15:25:55 +0000

    d

--- app/product/[productId]/ProductDetails.tsx
+++ app/product/[productId]/ProductDetails.tsx
@@ -1,8 +1,9 @@
 "use client";
+
 import Button from "@/app/components/Button";
 import ProductImage from "@/app/components/products/ProductImage";
 import SetColor from "@/app/components/products/SetColor";
-import SetQuantity from "@/app/components/products/SetQuantity";
+import SetQuatity from "@/app/components/products/SetQuantity";
 import { useCart } from "@/hooks/useCart";
 import { Rating } from "@mui/material";
 import { useRouter } from "next/navigation";
@@ -61,7 +62,7 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
         setIsProductInCart(true);
       }
     }
-  }, [cartProducts, product]);
+  }, [cartProducts]);
 
   const productRating =
     product.reviews.reduce((acc: number, item: any) => item.rating + acc, 0) /
@@ -96,106 +97,69 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
     });
   }, [cartProduct]);
 
-  const shouldRenderImageDiv = product.id === "659d3edcd7229f8f6a87903e";
-
   return (
-    <div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
-        <ProductImage
-          cartProduct={cartProduct}
-          product={product}
-          handleColorSelect={handleColorSelect}
-        />
-        <div className="flex flex-col gap-1 text-slate-500 text-sm">
-          <h2 className="text-3xl font-medium text-slate-700">
-            {product.name}
-          </h2>
-          <div className="flex items-center gap-2">
-            <Rating value={productRating} readOnly />
-            <div>{product.reviews.length} reviews</div>
-          </div>
-          <Horizontal />
-          <div className="text-justify">{product.description}</div>
-          <Horizontal />
-          <div>
-            <span className="font-semibold">CATEGORY:</span> {product.category}
-          </div>
-          <div>
-            <span className="font-semibold">BRAND:</span> {product.brand}
-          </div>
-          <div className={product.inStock ? "text-teal-400" : "text-rose-400"}>
-            {product.inStock ? "In stock" : "Out of stock"}
-          </div>
-          <Horizontal />
-          {isProductInCart ? (
-            <>
-              <p className="mb-2 text-slate-500 flex items-center gap-1">
-                <MdCheckCircle className="text-teal-400" size={20} />
-                <span>Product added to cart</span>
-              </p>
-              <div className="max-w-[300px]">
-                <Button
-                  label="View Cart"
-                  outline
-                  onClick={() => {
-                    router.push("/cart");
-                  }}
-                />
-              </div>
-            </>
-          ) : (
-            <>
-              <SetColor
-                cartProduct={cartProduct}
-                images={product.images}
-                handleColorSelect={handleColorSelect}
-              />
-              <Horizontal />
-              <SetQuantity
-                cartProduct={cartProduct}
-                handleQtyIncrease={handleQtyIncrease}
-                handleQtyDecrease={handleQtyDecrease}
-              />
-              <Horizontal />
-              <div className="max-w-[300px]">
-                <Button
-                  label="Add To Cart"
-                  onClick={() => handleAddProductToCart(cartProduct)}
-                />
-              </div>
-            </>
-          )}
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
+      <ProductImage
+        cartProduct={cartProduct}
+        product={product}
+        handleColorSelect={handleColorSelect}
+      />
+      <div className="flex flex-col gap-1 text-slate-500 text-sm">
+        <h2 className="text-3xl font-medium text-slate-700">{product.name}</h2>
+        <div className="flex items-center gap-2">
+          <Rating value={productRating} readOnly />
+          <div>{product.reviews.length} reviews</div>
         </div>
-      </div>
-      <div className="w-full md:w-1/2 mt-4 md:mt-0 px-4">
-        {shouldRenderImageDiv && (
-          <div className="flex flex-wrap gap-4">
-            <img
-              src="/IMG_2660.jpg"
-              alt="secure"
-              className="w-32 h-32 rounded-md"
+        <Horizontal />
+        <div className="text-justify">{product.description}</div>
+        <Horizontal />
+        <div>
+          <span className="font-semibold">CATEGORY:</span> {product.category}
+        </div>
+        <div>
+          <span className="font-semibold">BRAND:</span> {product.brand}
+        </div>
+        <div className={product.inStock ? "text-teal-400" : "text-rose-400"}>
+          {product.inStock ? "In stock" : "Out of stock"}
+        </div>
+        <Horizontal />
+        {isProductInCart ? (
+          <>
+            <p className="mb-2 text-slate-500 flex items-center gap-1">
+              <MdCheckCircle className="text-teal-400" size={20} />
+              <span>Product added to cart</span>
+            </p>
+            <div className="max-w-[300px]">
+              <Button
+                label="View Cart"
+                outline
+                onClick={() => {
+                  router.push("/cart");
+                }}
+              />
+            </div>
+          </>
+        ) : (
+          <>
+            <SetColor
+              cartProduct={cartProduct}
+              images={product.images}
+              handleColorSelect={handleColorSelect}
             />
-            <img
-              src="/IMG_2661.jpg"
-              alt="secure"
-              className="w-32 h-32 rounded-md"
+            <Horizontal />
+            <SetQuatity
+              cartProduct={cartProduct}
+              handleQtyIncrease={handleQtyIncrease}
+              handleQtyDecrease={handleQtyDecrease}
             />
-            <img
-              src="/IMG_2652.jpg"
-              alt="secur"
-              className="w-32 h-32 rounded-md"
-            />
-            <img
-              src="/IMG_2653.jpg"
-              alt="secu"
-              className="w-32 h-32 rounded-md"
-            />
-            <img
-              src="/IMG_2654.jpg"
-              alt="sec"
-              className="w-32 h-32 rounded-md"
-            />
-          </div>
+            <Horizontal />
+            <div className="max-w-[300px]">
+              <Button
+                label="Add To Cart"
+                onClick={() => handleAddProductToCart(cartProduct)}
+              />
+            </div>
+          </>
         )}
       </div>
     </div>
