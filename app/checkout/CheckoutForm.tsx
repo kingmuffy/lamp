6a6a4d480420957b3ec577b20dc013933641ad18
Mac <kingmuffyatse@gmail.com>
2024-01-09 20:11:04 +0000

    D

--- app/checkout/CheckoutForm.tsx
+++ app/checkout/CheckoutForm.tsx
@@ -75,7 +75,7 @@ const CheckoutForm: React.FC<CheckoutFormProps> = ({
       <AddressElement
         options={{
           mode: "shipping",
-          allowedCountries: ["GB"],
+          allowedCountries: ["GB", "NL"],
         }}
       />
       <h2 className="font-semibold mt-4 mb-2">Payment Information</h2>
