48db2c082ea9addf654aab996d61ffca4cb5feae
Mac <kingmuffyatse@gmail.com>
2024-01-11 18:47:06 +0000

    D

--- app/api/rating/route.ts
+++ app/api/rating/route.ts
@@ -1,36 +1,47 @@
+import prisma from "@/libs/prismadb";
 import getCurrentUser from "@/actions/getCurrentUser";
 import { Review } from "@prisma/client";
 import { NextResponse } from "next/server";
+export async function POST(request: Request) {
+  const currentUser = await getCurrentUser();
 
+  if (!currentUser) {
+    return new Response("Forbidden", { status: 403 });
+  }
 
-export async function POST(request: Request){
-    const currentUser = await getCurrentUser();
+  const body = await request.json();
+  const { comment, rating, product, userId } = body;
 
-    if(!currentUser){
-        return NextResponse.error();
-    }
+  const deliveredOrder = currentUser?.orders.some(
+    (order) =>
+      order.products.find((item) => item.id === product.id) &&
+      order.deliveryStatus === "delivered"
+  );
 
-    const body = await request.json()
-    const {comment, rating, product, userId} = body;
+  const userReview = product?.reviews.find(
+    (review: Review) => review.userId === currentUser.id
+  );
 
-    const deliveredOrder = currentUser?.orders.some(order => order.products.find(item => item.id === product.id) && order.deliveryStatus === 'delivered')
+  if (userReview || !deliveredOrder) {
+    return new Response("Forbidden", { status: 403 });
+  }
 
-    const userReview = product?.reviews.find(((review: Review) =>{
-        return review.userId === currentUser.id
-    }))
-
-    if(userReview || !deliveredOrder){
-        return NextResponse.error()
-    }
-
+  try {
     const review = await prisma?.review.create({
-        data: {
-            comment,
-            rating,
-            productId: product.id,
-            userId
-        }
-    })
+      data: {
+        comment,
+        rating,
+        productId: product.id,
+        userId,
+      },
+    });
 
-    return NextResponse.json(review)
+    return new Response(JSON.stringify(review), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    console.error("Error creating review:", error);
+    return new Response("Internal Server Error", { status: 500 });
+  }
 }
--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -12,7 +12,7 @@ const Footer = () => {
   return (
     <footer className="bg-slate-700 text-slate-200 text-sm mt-16">
       <Container>
-        <div className="flex flex-col md:flex-row justify-between pt-16 pb-8">
+        <div className="flex flex-col md:flex-row justify-between space-evenly pt-16 pb-8">
           <FooterList>
             <h3 className="text-base font-bold mb-2">Shop Categories</h3>
             <Link href="#">Phones</Link>
@@ -31,10 +31,9 @@ const Footer = () => {
           </FooterList>
           <div className="w-full md:w-1/3 mb-6 md:mb-0">
             <h3 className="text-base font-bold mb-2">About Us</h3>
-            <p className="mb-2">
+            <p className="mb-2 ">
               At our electronics store, we are dedicated to providing the latest
-              and greatest devices and accessories to our customers. With a wide
-              selection of phones, TVs, laptops, watches, and accessories.
+              and greatest devices and accessories to our customers.
             </p>
             <p>
               &copy; {new Date().getFullYear()} Ree~Store. All rights reserved
@@ -42,7 +41,7 @@ const Footer = () => {
           </div>
           <FooterList>
             <h3 className="text-base font-bold mb-2">Follow Us</h3>
-            <div className="flex gap-2">
+            <div className="flex gap-2 ">
               <Link href="#">
                 <MdFacebook size={24} />
               </Link>
@@ -52,11 +51,9 @@ const Footer = () => {
               <Link href="#">
                 <AiFillInstagram size={24} />
               </Link>
-              <Link href="#">
-                <AiFillYoutube size={24} />
-              </Link>
             </div>
           </FooterList>
+          <img src="/frf.jpg" alt="Logo" className="w-64 h-24 " />
         </div>
       </Container>
     </footer>
